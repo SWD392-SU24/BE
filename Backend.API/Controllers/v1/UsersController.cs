b444a3c5ed65cf39bc1b8e1eee441c21cfa31813
Long Vu <vulongbinhduong@gmail.com>
2024-06-03 20:09:07 +0700

    Feature/dt 22 (#4)
    * feat: add register api for customer and clinic owner
    
    * fix: change logical comparision in authenticate function

---
+++ Backend.API/Controllers/v1/UsersController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Backend.BO.Commons;
+using Backend.BLL.Features.Users;
+using Backend.BO.Payloads.Requests;
+using System.Net;
+
+namespace Backend.API.Controllers.v1
+{
+    public class UsersController : BaseApiController
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET: api/Users
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        //{
+        //    return await _context.Users.ToListAsync();
+        //}
+
+        // GET: api/Users/5
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<User>> GetUser(Guid id)
+        //{
+        //    var user = await _context.Users.FindAsync(id);
+
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return user;
+        //}
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutUser(Guid id, User user)
+        //{
+        //    if (id != user.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    _context.Entry(user).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!UserExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return NoContent();
+        //}
+
+        
+        [HttpPost("sign-up/customer")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesErrorResponseType(typeof(ResponseModel<string>))]
+        public async Task<ActionResult<User>> SignupForCustomer(CustomerSignupRequest customer)
+        {
+            var result = await _userService.SignupForCustomer(customer);
+            if (result)
+            {
+                var response = new ResponseModel<string>(
+                    statusCode: (int)HttpStatusCode.OK,
+                    message: "Register successfully!",
+                    response: null
+                );
+                return Ok(response);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("sign-up/clinic-owner")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesErrorResponseType(typeof(ResponseModel<string>))]
+        public async Task<ActionResult<User>> SignupForClinicOwner(ClinicOwnerSignupRequest clinicOwner)
+        {
+            var result = await _userService.SignupForClinicOwner(clinicOwner);
+            if (result)
+            {
+                var response = new ResponseModel<string>(
+                    statusCode: (int)HttpStatusCode.OK,
+                    message: "Register successfully!",
+                    response: null
+                );
+                return Ok(response);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        // DELETE: api/Users/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteUser(Guid id)
+        //{
+        //    var user = await _context.Users.FindAsync(id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.Users.Remove(user);
+        //    await _context.SaveChangesAsync();
+
+        //    return NoContent();
+        //}
+
+        //private bool UserExists(Guid id)
+        //{
+        //    return _context.Users.Any(e => e.Id == id);
+        //}
+    }
+}
---
+++ Backend.BO/Payloads/Requests/ClinicOwnerSignupRequest.cs
@@ -0,0 +1,21 @@
+﻿using Backend.BO.Constants;
+
+namespace Backend.BO.Payloads.Requests
+{
+    public class ClinicOwnerSignupRequest
+    {
+        public required string Email { get; set; }
+
+        public required string FirstName { get; set; }
+
+        public required string LastName { get; set; }
+
+        public required string Password { get; set; }
+
+        public string? CitizenId { get; set; }
+
+        public required string ConfirmedPassword { get; set; }
+
+        public string Role { get; } = UserRole.ClinicOwner;
+    }
+}
